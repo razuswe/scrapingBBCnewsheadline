5d838bf04a450936106802e77f229bedcf903822
Razu Ahmed <rarony009@gmail.com>
2019-03-18 15:13:18 +0600

    Update commit m_scrap

--- m_scrap.py
+++ m_scrap.py
@@ -13,7 +13,7 @@ w_log.set_custom_log_info('html/error.log')
 
 news_scrap = w_scrap.NewsScraper(w_scrap.url_ds,w_log)
 news_scrap.retrieve_webpage()
-news_scrap.write_webpage_as_html() #by default: daily star
+news_scrap.write_webpage_as_html() #by default: BBC News
 news_scrap.read_webpage_as_html()
 news_scrap.convert_data_to_bs4()
 news_scrap.parse_soup_to_simple_html()
